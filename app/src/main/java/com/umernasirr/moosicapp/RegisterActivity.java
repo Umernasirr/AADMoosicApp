4c472874f91090e187d4b95a433ae12551b75190
asbah jaffri <ajaffri44@gmail.com>
2020-12-17 03:04:12 +0500

    test commit

--- app/src/main/java/com/umernasirr/moosicapp/RegisterActivity.java
+++ app/src/main/java/com/umernasirr/moosicapp/RegisterActivity.java
@@ -11,5 +11,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+    //lmao xd
 
 }
