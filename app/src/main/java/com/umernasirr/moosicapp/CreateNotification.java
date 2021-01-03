3e73cda2c8618f50add5f986ecf4624bbd5219ab
asbah jaffri <ajaffri44@gmail.com>
2021-01-03 21:18:32 +0500

    donesiez

---
+++ app/src/main/java/com/umernasirr/moosicapp/CreateNotification.java
@@ -0,0 +1,18 @@
+package com.umernasirr.moosicapp;
+
+import android.app.Notification;
+import android.content.Context;
+
+public class CreateNotification {
+
+    public static final String CHANNEL_ID = "channel1";
+    public static final String ACTIONPREVIOUS  =   "actionprevious";
+    public static final String CHANNEL_PLAY = "actionplay";
+    public static final String CHANNEL_NEXT = "actionnext";
+
+    public static Notification notification;
+
+    public static void createNotification(Context context )
+
+
+}
--- app/src/main/java/com/umernasirr/moosicapp/MusicPlayerActivity.java
+++ app/src/main/java/com/umernasirr/moosicapp/MusicPlayerActivity.java
@@ -173,12 +173,15 @@ public class MusicPlayerActivity extends AppCompatActivity {
     }
     @SuppressLint("DefaultLocale")
     private String convertFormat(int duration){
-      return String.format("%02:%02" , TimeUnit.MILLISECONDS.toMinutes(duration),
+      return String.format("%02d:%02d" , TimeUnit.MILLISECONDS.toMinutes(duration),
               TimeUnit.MILLISECONDS.toSeconds(duration),
               TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
 
 
 
     }
+
+
+
 
 }
